c7786b74d13e2c153a2c8afcf8b496a7094dedcc
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-06 17:00:59 -1000

    testing setup complete

--- shopping_list/test/test_spec.js
+++ shopping_list/test/test_spec.js
@@ -1,8 +1,18 @@
 var should = chai.should();
 
-describe('Shopping List', function () {
+describe('Behavior Driven Shopping List', function () {
 
   describe('Shopping List Item', function () {
+
+    beforeEach(function () {
+      var apple = new ShoppingListItem('apple');
+    });
+
+    it('Shopping List Item is a class', function (done) {
+      Shopping_List_Item.should.be.a.function;
+    });
+
+
 
   });
 
