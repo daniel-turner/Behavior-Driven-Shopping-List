6a0f98a147e828bedc13206c7dbd1f58e853fa41
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-06 19:10:18 -1000

    partial setup of the testing for the shopping list

--- shopping_list/test/test_spec.js
+++ shopping_list/test/test_spec.js
@@ -96,6 +96,144 @@ describe('Behavior Driven Shopping List', function () {
 
   describe('Shopping List', function () {
 
+
+          describe('Shopping List', function () {
+
+
+              var listy;
+               beforeEach(function () {
+                 listy = new ShoppingList('Food');
+               });
+
+
+              it('ShoppingList is a function', function () {
+                ShoppingList.should.be.a('function');
+              });
+
+              it('ShoppingList has a property named items', function() {
+                ShoppingList.should.have.property('items');
+              });
+
+
+
+
+
+              describe('Constructor', function(){
+              it('ShoppingList Items should be equal to the argument passed \'Food\'', function () {
+                lisyt.items.should.be.equal.to('Food');
+              });
+
+
+              describe('Methods', function(){
+
+                describe('addItem', function () {
+
+
+                  it('addItem should be a bunction', function (done) {
+                    addItem.should.be.a.function;
+                  });
+
+                });
+
+                describe('removeItem', function () {
+                  it('removeItem should be a bunction', function (done) {
+                    addItem.should.be.a.function;
+                  });
+                });
+
+                describe('render', function () {
+                  it('render should be a bunction', function (done) {
+                    addItem.should.be.a.function;
+                  });
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              });
+
+
+
+
+
+              // describe('check', function() {
+
+              //   it('check method exists', function() {
+
+              //     apple.check.should.be.a('function');
+              //   })
+
+              //   it('check sets is_done to true', function() {
+
+              //     apple.check();
+              //     apple.is_done.should.be.true;
+              //   });
+              // });
+
+              // describe('uncheck', function() {
+
+              //   it('uncheck method exists', function() {
+
+              //     apple.uncheck.should.be.a('function');
+              //   });
+
+              //   it('uncheck sets is_done to false', function() {
+
+              //     apple.uncheck();
+              //     apple.is_done.should.be.false;
+              //   });
+              // });
+
+              // describe('render', function() {
+
+              //   it('render method exists', function() {
+
+              //     apple.render.should.be.a('function');
+              //   });
+
+              //   it('render should return a string', function() {
+
+              //     apple.render().should.be.a('string');
+              //   });
+
+              //   it('render should return a string wrapped in <ul>s', function() {
+
+              //     apple.render().indexOf('<ul>').should.be.above(-1);
+              //     apple.render().lastIndexOf('</ul>').should.be.above(-1);
+
+              //   });
+
+              // });
+
+
+
+
+
+
+
+
+
+
   });
 
 
