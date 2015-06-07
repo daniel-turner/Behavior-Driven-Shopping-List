33fcea8a3a36b7fee922055f91c28087c33e8dde
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-06 16:34:29 -1000

    initial setup of spec file

--- shopping_list/test/test_spec.js
+++ shopping_list/test/test_spec.js
@@ -1,2 +1,18 @@
 var should = chai.should();
 
+describe('Shopping List', function () {
+
+  describe('Shopping List Item', function () {
+
+  });
+
+
+  describe('Shopping List', function () {
+
+  });
+
+
+
+
+
+});
