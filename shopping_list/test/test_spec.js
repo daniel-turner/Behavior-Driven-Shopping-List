0ed61955e0236feca06d238c5639f55e4410cd86
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-06 19:40:57 -1000

    draft of test file

--- shopping_list/test/test_spec.js
+++ shopping_list/test/test_spec.js
@@ -116,9 +116,6 @@ describe('Behavior Driven Shopping List', function () {
               });
 
 
-
-
-
               describe('Constructor', function(){
               it('ShoppingList Items should be equal to the argument passed \'Food\'', function () {
                 lisyt.items.should.be.equal.to('Food');
@@ -127,41 +124,74 @@ describe('Behavior Driven Shopping List', function () {
 
               describe('Methods', function(){
 
+                var plum;
+
+                before(function () {
+                  plum = new ShoppingListItem('plum','the softer the sweeter!');
+                });
                 describe('addItem', function () {
                   it('addItem should be a function', function () {
                     listy.addItem.should.be.a.function;
                   });
-                  it('should only pass in items that are a ShoppingListItem', function (items) {
-                    items.should.should.be.an.instanceof(ShoppingListItem);
+                  it('should only pass in items that are a ShoppingListItem', function () {
+                    plum.should.be.an.instanceof(ShoppingListItem);
                   });
                   it('should pass a listItem into the items array', function () {
                     listy.items.should.be.should.have.length.above(0);
+                    list.items[0].name.should.equal('plum');
+                    listy.addItem(plum);
+                    listy.items.should.be.should.have.length.above(1);
+                    listy.addItem(plum);
+                    listy.items.should.be.should.have.length.above(2);
                   });
 
 
+                });
+
+                var plum;
+                var peach;
+
+                before(function () {
+                  plum = new ShoppingListItem('plum','the softer the sweeter!');
+                  peach = new ShoppingListItem('plum','the softer the sweeter!');
                 });
 
                 describe('removeItem', function () {
                   it('removeItem should be a function', function () {
                     listy.addItem.should.be.a.function;
                   });
-                  it('should remove an item from the shopping list ', function (items) {
-
-
-
-
-
+                  it('should remove an item from the shopping list ', function () {
+                //adds a plum, checks the length, removes the plum, checks the length
+                    listy.addItem(plum);
+                    listy.items.should.contain(plum);;
+                    listy.removeItem(plum)
+                    listy.items.should.have.length(0);
                   });
-
-
-
-
+                  it('should remove the last item if no parameters are given', function (done) {
+                    //adds plum and a peach, removes peach (its at the end)
+                    listy.addItem(plum);
+                    listy.addItem(peach);
+                    listy.removeItem();
+                    listy.items.should.not.contain(peach);
+                  });
+                  it('should only remove items that are on the list', function (done) {
+                    var candle = 'candle';
+                    listy.addItem(plum);
+                    listy.addItem(peach);
+                    listy.removeItem(candle);
+                    listy.items.should.have.length(2);
+                  });
                 });
 
                 describe('render', function () {
                   it('render should be a function', function () {
                     listy.addItem.should.be.a.function;
                   });
+                  it('should concat the results in a ul list tag', function (done) {
+                    apple.render().indexOf('<ul>').should.be.above(-1);
+                    apple.render().lastIndexOf('</ul>').should.be.above(-1);
+                  });
+
                 });
 
 
