70594a2a7356493bc9aba3d36d5410cb54292dd1
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-06 18:44:01 -1000

    made soem mods to the shopping_list

--- shopping_list/js/shopping_list.js
+++ shopping_list/js/shopping_list.js
@@ -4,16 +4,17 @@ function ShoppingListItem (name, description) {
   this.is_done = null;
 
   this.check = function(){
-    is_done = true;
+    this.is_done = true;
   }
   this.uncheck = function(){
-    is_done = false
+    this.is_done = false;
   }
   this.render = function(){
     var sentence = '<li class =\"completed_' + this.is_done + '\"> \
                       <span>' + this.name + '</span> \
                       <span>' + this.description + '</span>\
                     </li>';
+    return sentence;
   }
 };
 
