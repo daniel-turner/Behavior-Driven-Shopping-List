0b84de721d7b802b15957e550158eca6da9ea9fc
danturne <danturne@yahoo.com>
2015-06-08 10:50:56 -1000

    separated shoppinglistitem.js

--- shopping_list/js/shopping_list.js
+++ shopping_list/js/shopping_list.js
@@ -1,24 +1,3 @@
-function ShoppingListItem (name, description) {
-  this.name = name;
-  this.description = description;
-  this.is_done = null;
-
-  this.check = function(){
-    this.is_done = true;
-  }
-  this.uncheck = function(){
-    this.is_done = false;
-  }
-  this.render = function(){
-    var sentence = '<li class =\"completed_' + this.is_done + '\"> \
-                      <span>' + this.name + '</span> \
-                      <span>' + this.description + '</span>\
-                    </li>';
-    //console.log(sentence);
-    return sentence;
-  }
-};
-
 function Shopping_List() {
 
   this.items = [];
